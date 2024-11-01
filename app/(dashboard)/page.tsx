b05cd88174c4672acf4b12af8f1dd3ec4886649f
EdLoth <eduardosram10@gmail.com>
2024-11-01 09:22:28 -0300

    🔒 feat: adjust authentication pages and middleware

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,8 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home(){
   return(
-    <Button>
-      This is an authenticated route
-    </Button>
+    <UserButton afterSignOutUrl="/sign-in" />
   )
 }
